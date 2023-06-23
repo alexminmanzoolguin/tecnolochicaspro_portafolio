6dc5aedfd35498a765132aea44f582ddfaa7bacb
Alex Yazmin Manzo olguin <ada_hack418@hackademy.mx>
2023-06-23 14:43:00 -0600

    cambio
    solo cambio de texto

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Analista Desarrolador jr en la empresa Manpowergroup y desarrolladora jr')
+  .typeString('Analista Desarrolador jr y desarrolladora jr')
   .pauseFor(200)
   .deleteChars(10)
   .start();
